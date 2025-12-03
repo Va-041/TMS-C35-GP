ad162cebf3a017a63686a2c7c56368e2f133ab05
Va-041 <vladyaserika@mail.ru>
2025-12-03 18:15:55 +0300

    Init project commit

---
+++ src/main/java/org/funquizzes/tmsc35gp/TmsC35GpApplication.java
@@ -0,0 +1,13 @@
+package org.funquizzes.tmsc35gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsC35GpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsC35GpApplication.class, args);
+    }
+
+}
