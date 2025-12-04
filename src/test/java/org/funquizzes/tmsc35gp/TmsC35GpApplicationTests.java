8e79f9bc593ef5e1e7c44557a32752708234e4a1
Va-041 <vladyaserika@mail.ru>
2025-12-04 18:20:28 +0300

    Create and add index page style

---
+++ src/test/java/org/funquizzes/tmsc35gp/TmsC35GpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.funquizzes.tmsc35gp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsC35GpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
