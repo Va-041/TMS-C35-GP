3dae007c6be8ba7889b0af901117e45796fb1db6
Va-041 <vladyaserika@mail.ru>
2025-12-18 00:33:54 +0300

    update rating system finally

---
+++ src/main/java/org/funquizzes/tmsc35gp/dto/UpdateQuizRatingDto.java
@@ -0,0 +1,20 @@
+package org.funquizzes.tmsc35gp.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateQuizRatingDto {
+
+    @NotNull(message = "Оценка обязательна")
+    @Min(value = 1, message = "Оценка должна быть от 1 до 5")
+    @Max(value = 5, message = "Оценка должна быть от 1 до 5")
+    private Double rating;
+
+    @Size(max = 500, message = "Комментарий не должен превышать 500 символов")
+    private String comment;
+}
